2b2414695dc7552fb1cc0a3c333f224c5a6f0385
Pavel13E <pavel.elashkin@gmail.com>
2021-12-23 01:59:31 +0300

    Добавьте файлы проекта.

---
+++ Wpfbase_Lesson9/MainWindow.xaml.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpfbase_Lesson9
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<string> styles = new List<string>() { "Светлая тема", "Темная тема" };
+            styleBox.ItemsSource = styles;
+            styleBox.SelectionChanged +=Themereverse;
+            styleBox.SelectedIndex = 0;
+
+        }
+
+        private void Themereverse(object sender, SelectionChangedEventArgs e)
+        {
+            int styleIndex = styleBox.SelectedIndex;
+            Uri uri = new Uri("Dictionary2Light.xaml",UriKind.Relative);
+            if (styleIndex==1)
+            {
+                uri = new Uri("Dictionary1dark.xaml", UriKind.Relative);
+            }
+            ResourceDictionary resource = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resource);
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = (sender as ComboBox).SelectedItem as String;
+
+            //string fontName = ((sender as ComboBox).SelectedItem as TextBlock);
+            if (textbox != null)
+            {
+                textbox.FontFamily = new FontFamily(fontName);
+            }
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            string fontSize = (sender as ComboBox).SelectedItem as String;
+            double forntSizeconv = Convert.ToDouble(fontSize);
+            if (textbox != null)
+            {
+                textbox.FontSize = forntSizeconv;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                if (textbox.FontWeight == FontWeights.Normal)
+                {
+                    textbox.FontWeight = FontWeights.Bold;
+                }
+                else
+                {
+                    textbox.FontWeight = FontWeights.Normal;
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (textbox.FontStyle == FontStyles.Normal)
+            {
+                textbox.FontStyle = FontStyles.Italic;
+            }
+            else
+            {
+                textbox.FontStyle = FontStyles.Normal;
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (textbox.TextDecorations == TextDecorations.Baseline)
+            {
+                textbox.TextDecorations = TextDecorations.Underline;
+            }
+            else
+            {
+                textbox.TextDecorations = TextDecorations.Baseline;
+            }
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            textbox.Foreground = Brushes.Black;
+        }
+
+        private void RadioButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            textbox.Foreground = Brushes.Red;
+        }
+
+        private void ExitExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ClosedExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            textbox.Clear();
+        }
+
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовый файл (*.txt)|*.txt|Все файлы(*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textbox.Text);
+            }
+        }
+
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовый файл (*.txt)|*.txt|Все файлы(*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textbox.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void RadioButton_Click_2(object sender, RoutedEventArgs e)
+        {
+            textbox.Foreground = Brushes.White;
+        }
+    }
+}
---
+++ Wpfbase_Lesson9/MyCommands1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Wpfbase_Lesson9
+{
+    class MyCommands1
+    {
+        public static RoutedCommand Exit { get; set; }
+        public static RoutedUICommand Closed { get; set; }
+        public static RoutedUICommand Save { get; set; }
+        public static RoutedUICommand Open { get; set; }
+        static MyCommands1()
+        {
+            Exit = new RoutedCommand();
+
+            InputGestureCollection Input1 = new InputGestureCollection();
+            Input1.Add(new KeyGesture(Key.C, ModifierKeys.Control, "Ctrl+C"));
+            Closed = new RoutedUICommand("_Закрыть", "Closed", typeof(MyCommands1), Input1);
+
+            InputGestureCollection Input2 = new InputGestureCollection();
+            Input2.Add(new KeyGesture(Key.S, ModifierKeys.Control, "Ctrl+S"));
+            Save = new RoutedUICommand("_Сохранить", "Save", typeof(MyCommands1), Input2);
+
+            InputGestureCollection Input3 = new InputGestureCollection();
+            Input3.Add(new KeyGesture(Key.O, ModifierKeys.Control, "Ctrl+O"));
+            Open = new RoutedUICommand("_Открыть", "Open", typeof(MyCommands1), Input3);
+        }
+    }
+}
